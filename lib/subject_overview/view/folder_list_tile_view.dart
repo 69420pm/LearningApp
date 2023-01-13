71f92f3f000ea84e1a9eace923649f80e89b34e1
luazars <werbungmaef@gmail.com>
2023-01-13 15:49:25 +0100

    slidable folders but dragging of folders doesnt work any more so very bad

--- lib/subject_overview/view/folder_list_tile_view.dart
+++ lib/subject_overview/view/folder_list_tile_view.dart
@@ -1,6 +1,7 @@
 import 'package:cards_api/cards_api.dart';
 import 'package:flutter/material.dart' hide Card;
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:learning_app/subject_overview/bloc/folder_bloc/folder_list_tile_bloc.dart';
 
 import 'package:learning_app/subject_overview/view/card_list_tile.dart';
@@ -22,31 +23,26 @@ class FolderListTileView extends StatelessWidget {
   final Map<String, Widget> childListTiles;
   @override
   Widget build(BuildContext context) {
-    return ExpansionTile(
-      controlAffinity: ListTileControlAffinity.leading,
-
-      collapsedTextColor: Theme.of(context).colorScheme.onSecondaryContainer,
-      textColor: Theme.of(context).colorScheme.onSecondaryContainer,
-
-      maintainState: true,
-
-      title: Text(folder.name),
-
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
+    return Slidable(
+      key: Key(folder.id),
+      startActionPane: ActionPane(
+        motion: const StretchMotion(),
         children: [
-          IconButton(
-            icon: const Icon(Icons.delete),
-            onPressed: () => context.read<FolderListTileBloc>().add(
+          SlidableAction(
+            onPressed: (context) => context.read<FolderListTileBloc>().add(
                   FolderListTileDeleteFolder(
                     id: folder.id,
                     parentId: folder.parentId,
                   ),
                 ),
+            icon: Icons.delete,
+            label: 'delete',
+            backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+            borderRadius: const BorderRadius.horizontal(
+                left: Radius.circular(UISizeConstants.cornerRadius)),
           ),
-          IconButton(
-            icon: const Icon(Icons.flutter_dash),
-            onPressed: () {
+          SlidableAction(
+            onPressed: (context) {
               for (var i = 0; i <= 20; i++) {
                 context.read<FolderListTileBloc>().add(
                       FolderListTileAddCard(
@@ -65,43 +61,109 @@ class FolderListTileView extends StatelessWidget {
                     );
               }
             },
+            icon: Icons.cruelty_free,
+            label: '20 cards',
+            backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
           ),
         ],
       ),
-      //
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(
-            left: UISizeConstants.defaultSize * 2,
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              if (childListTiles.values.whereType<FolderListTile>().isNotEmpty)
-                ListView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount:
-                      childListTiles.values.whereType<FolderListTile>().length,
-                  itemBuilder: (context, index) => childListTiles.values
-                      .whereType<FolderListTile>()
-                      .elementAt(index),
-                ),
-              if (childListTiles.values.whereType<CardListTile>().isNotEmpty)
-                ListView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount:
-                      childListTiles.values.whereType<CardListTile>().length,
-                  itemBuilder: (context, index) => childListTiles.values
-                      .whereType<CardListTile>()
-                      .elementAt(index)
-                    ..isInSelectMode = inSelectionMode,
-                ),
-            ],
-          ),
+      child: ExpansionTile(
+        controlAffinity: ListTileControlAffinity.leading,
+
+        collapsedTextColor: Theme.of(context).colorScheme.onSecondaryContainer,
+        textColor: Theme.of(context).colorScheme.onSecondaryContainer,
+
+        maintainState: true,
+
+        title: Row(
+          children: [
+            const Icon(Icons.folder),
+            const SizedBox(
+              width: UISizeConstants.defaultSize * 1,
+            ),
+            Expanded(
+              child: Text(
+                folder.name,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ],
         ),
-      ],
+
+        // trailing: Row(
+        //   mainAxisSize: MainAxisSize.min,
+        //   children: [
+        //     IconButton(
+        //       icon: const Icon(Icons.delete),
+        //       onPressed: () => context.read<FolderListTileBloc>().add(
+        //             FolderListTileDeleteFolder(
+        //               id: folder.id,
+        //               parentId: folder.parentId,
+        //             ),
+        //           ),
+        //     ),
+        //     IconButton(
+        //       icon: const Icon(Icons.flutter_dash),
+        //       onPressed: () {
+        //         for (var i = 0; i <= 20; i++) {
+        //           context.read<FolderListTileBloc>().add(
+        //                 FolderListTileAddCard(
+        //                   card: Card(
+        //                     back: 'test$i',
+        //                     front: 'test$i',
+        //                     askCardsInverted: false,
+        //                     id: const Uuid().v4(),
+        //                     dateCreated: '',
+        //                     parentId: folder.id,
+        //                     dateToReview: DateTime.now().toIso8601String(),
+        //                     typeAnswer: false,
+        //                   ),
+        //                   newParentId: folder.id,
+        //                 ),
+        //               );
+        //         }
+        //       },
+        //     ),
+        //   ],
+        // ),
+        //
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              left: UISizeConstants.defaultSize * 2,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                if (childListTiles.values
+                    .whereType<FolderListTile>()
+                    .isNotEmpty)
+                  ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: childListTiles.values
+                        .whereType<FolderListTile>()
+                        .length,
+                    itemBuilder: (context, index) => childListTiles.values
+                        .whereType<FolderListTile>()
+                        .elementAt(index),
+                  ),
+                if (childListTiles.values.whereType<CardListTile>().isNotEmpty)
+                  ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount:
+                        childListTiles.values.whereType<CardListTile>().length,
+                    itemBuilder: (context, index) => childListTiles.values
+                        .whereType<CardListTile>()
+                        .elementAt(index)
+                      ..isInSelectMode = inSelectionMode,
+                  ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
