259f6cfef5320c5cc2193974ecd414baf7f30e7f
luazars <werbungmaef@gmail.com>
2023-05-04 20:07:50 +0200

    very big code clean up, color row deleted --> colorpicker in progress

--- packages/markdown_editor/lib/src/widgets/keyboard_row/rows/keyboard_lower_row_text_tile.dart
+++ packages/markdown_editor/lib/src/widgets/keyboard_row/rows/keyboard_lower_row_text_tile.dart
@@ -1,11 +1,10 @@
-import 'dart:developer';
-
+// ignore_for_file: public_member_api_docs
 import 'package:flutter/material.dart';
 import 'package:markdown_editor/markdown_editor.dart';
-import 'package:markdown_editor/src/widgets/keyboard_row/keyboard_expandable.dart';
 import 'package:markdown_editor/src/widgets/keyboard_row/keyboard_toggle.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+// ignore: must_be_immutable
 class KeyboardLowerRowTextTile extends StatelessWidget {
   KeyboardLowerRowTextTile({super.key});
   bool isBold = false;
@@ -55,27 +54,23 @@ class KeyboardLowerRowTextTile extends StatelessWidget {
                 );
           },
         ),
-        KeyboardExpandable(
+        IconButton(
           icon: const Icon(Icons.format_color_text),
           onPressed: () => context.read<KeyboardRowCubit>().expandTextColors(),
         ),
-        KeyboardExpandable(
+        IconButton(
           icon: const Icon(Icons.functions),
-          onPressed: () {
-            context.read<KeyboardRowCubit>().expandExtraFormat();
-          },
+          onPressed: () => context.read<KeyboardRowCubit>().expandExtraFormat(),
         ),
         Container(
           width: 2,
           color: Theme.of(context).colorScheme.outline,
           height: 38,
         ),
-        KeyboardExpandable(
+        IconButton(
           icon: const Icon(Icons.add),
-          onPressed: (){
-            context.read<KeyboardRowCubit>().expandAddNewTextTile();
-          },
-          width: 70,
+          onPressed: () =>
+              context.read<KeyboardRowCubit>().expandAddNewTextTile(),
         ),
       ],
     );
