a8143fd8979aeb88ca8c92b0b341bae22c437efe
luazars <werbungmaef@gmail.com>
2023-06-06 19:26:06 +0200

    small changes

--- packages/ui_components/lib/src/widgets/app_bar.dart
+++ packages/ui_components/lib/src/widgets/app_bar.dart
@@ -4,7 +4,6 @@ import 'package:ui_components/ui_components.dart';
 class UIAppBar extends AppBar {
   UIAppBar({super.key, super.title, super.scrolledUnderElevation});
 
-  @override
   Widget build(BuildContext context) {
     return PreferredSize(
       preferredSize: const Size.fromHeight(UIConstants.defaultSize * 4),
