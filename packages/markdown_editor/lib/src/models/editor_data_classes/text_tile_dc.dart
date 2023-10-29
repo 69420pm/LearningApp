b7021e0a2b557e47fabd3c3766182deb1ad4ded0
flopp <hackeranonym@protonmail.com>
2023-10-29 21:10:41 +0100

    fixed saveCard

--- packages/markdown_editor/lib/src/models/editor_data_classes/text_tile_dc.dart
+++ packages/markdown_editor/lib/src/models/editor_data_classes/text_tile_dc.dart
@@ -1,10 +1,13 @@
+import 'package:markdown_editor/src/models/char_tile.dart';
 import 'package:markdown_editor/src/models/editor_data_classes/editor_tile_dc.dart';
 
+/// matching data class to TextTile
 class TextTileDC extends EditorTileDC {
   TextTileDC({required super.uid});
 
-  // Map<int, CharTile> charTiles = {};
+  // charTiles of text
+  Map<int, CharTile> charTiles = {};
 
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [charTiles];
 }
