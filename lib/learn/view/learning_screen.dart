004da928150480ec16ec4e079165a337dbb32b1f
luazars <werbungmaef@gmail.com>
2023-12-30 14:20:53 +0100

    google shit package has bug (after some time, it breakes between card 1 and 2 )

--- lib/learn/view/learning_screen.dart
+++ lib/learn/view/learning_screen.dart
@@ -7,6 +7,7 @@ import 'package:learning_app/card_backend/cards_repository.dart';
 import 'package:learning_app/learn/cubit/learn_cubit.dart';
 import 'package:learning_app/learn/view/learning_card.dart';
 import 'package:learning_app/ui_components/ui_colors.dart';
+import 'package:learning_app/ui_components/ui_text.dart';
 import 'package:learning_app/ui_components/widgets/ui_appbar.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
@@ -28,65 +29,74 @@ class _LearningScreenState extends State<LearningScreen> {
       ScrollOffsetListener.create();
 
   int currentIndex = 0;
+  bool scrollToBottom = false;
   bool inScroll = false;
 
   @override
   Widget build(BuildContext context) {
-    final visibleCards = itemPositionsListener.itemPositions.value.toList();
-    const minDrag = 0.1;
-
-    //
-
+    final screenHeight = MediaQuery.of(context).size.height;
+    final heights = [
+      screenHeight,
+      screenHeight * 1.2,
+      screenHeight * 2,
+      screenHeight * 4,
+      screenHeight
+    ];
     return Scaffold(
       backgroundColor: UIColors.background,
-      appBar: const UIAppBar(
-        leadingBackButton: true,
-        title: 'Learning Site',
-      ),
+      // appBar: const UIAppBar(
+      //   leadingBackButton: true,
+      //   title: 'Learning Site',
+      // ),
       body: NotificationListener<UserScrollNotification>(
         onNotification: (notification) {
-          if (notification.direction == ScrollDirection.idle) {
-            final scrollRatio = itemPositionsListener
-                .itemPositions.value.first.itemTrailingEdge
-                .clamp(0, 1);
-            print(scrollRatio);
-            if (scrollRatio > 1 - minDrag) {
-              setState(() {
-                currentIndex =
-                    itemPositionsListener.itemPositions.value.first.index;
-              });
-            } else if (scrollRatio < minDrag) {
-              setState(() {
-                currentIndex =
-                    itemPositionsListener.itemPositions.value.last.index;
-              });
-            } else {
-              setState(() {
-                currentIndex = itemPositionsListener.itemPositions.value
-                    .elementAt(scrollRatio < .5 ? 0 : 1)
-                    .index;
-              });
-            }
+          // if (notification.direction == ScrollDirection.idle) {
 
-            if (scrollRatio != 1) {
-              itemScrollController.scrollTo(
-                  index: currentIndex,
-                  duration: Duration(milliseconds: 400),
-                  alignment: scrollRatio < .5 ? 0 : 0);
-            }
+          var scrollRatio =
+              itemPositionsListener.itemPositions.value.first.itemTrailingEdge;
+
+          if (scrollRatio > 0.5) {
+            setState(() {
+              currentIndex =
+                  itemPositionsListener.itemPositions.value.first.index;
+              scrollToBottom = true;
+            });
+          } else {
+            setState(() {
+              currentIndex =
+                  itemPositionsListener.itemPositions.value.last.index;
+            });
+            scrollToBottom = false;
           }
+
+          // if (scrollRatio != 1) {
+          //   itemScrollController.jumpTo(index: currentIndex, alignment: -0.5);
+          // }
+          // }
           return true;
         },
         child: ScrollablePositionedList.builder(
+          padding: EdgeInsets.all(10),
           itemScrollController: itemScrollController,
           scrollOffsetController: scrollOffsetController,
           itemPositionsListener: itemPositionsListener,
           scrollOffsetListener: scrollOffsetListener,
           itemCount: 5,
           itemBuilder: (context, index) {
-            return Placeholder(
-              fallbackHeight: MediaQuery.of(context).size.height * 1.5,
-              color: index == currentIndex ? Colors.red : Colors.blue,
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: Container(
+                height: screenHeight * 3,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  color: index == currentIndex ? Colors.red : Colors.blue,
+                ),
+                child: Center(
+                    child: Text(
+                  index.toString(),
+                  style: UIText.titleBig,
+                )),
+              ),
             );
           },
         ),
