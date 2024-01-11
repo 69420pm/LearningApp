c13625896314c5ff0237aad3f38238419f8cbeb0
69420pm <420dev69420@gmail.com>
2024-01-11 21:16:58 +0100

    calendar finished

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -9,17 +9,24 @@ import 'package:flutter/material.dart' hide Card;
 import 'package:hive/hive.dart';
 import 'package:learning_app/app/app.dart';
 import 'package:learning_app/bootstrap.dart';
+import 'package:learning_app/calendar_backend/calendar_api/calendar_api.dart';
+import 'package:learning_app/calendar_backend/calendar_api/models/calendar_day.dart';
+import 'package:learning_app/calendar_backend/calendar_repository.dart';
+import 'package:learning_app/calendar_backend/hive_calendar_api.dart';
 import 'package:learning_app/card_backend/cards_api/models/card.dart';
 import 'package:learning_app/card_backend/cards_api/models/class_test.dart';
 import 'package:learning_app/card_backend/cards_api/models/folder.dart';
 import 'package:learning_app/card_backend/cards_api/models/subject.dart';
 import 'package:learning_app/card_backend/cards_repository.dart';
 import 'package:learning_app/card_backend/hive_cards_api.dart';
+import 'package:learning_app/editor/models/editor_data_classes/front_back_seperator_tile_dc.dart';
+import 'package:learning_app/ui_components/backend/hive_ui_api.dart';
+import 'package:learning_app/ui_components/backend/ui_repository.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 import 'package:learning_app/editor/models/editor_data_classes/audio_tile_dc.dart';
 import 'package:learning_app/editor/models/editor_data_classes/callout_tile_dc.dart';
 import 'package:learning_app/editor/models/editor_data_classes/char_tile_dc.dart';
 import 'package:learning_app/editor/models/editor_data_classes/divider_tile_dc.dart';
-import 'package:learning_app/editor/models/editor_data_classes/editor_tile_dc.dart';
 import 'package:learning_app/editor/models/editor_data_classes/header_tile_dc.dart';
 import 'package:learning_app/editor/models/editor_data_classes/image_tile_dc.dart';
 import 'package:learning_app/editor/models/editor_data_classes/latex_tile_dc.dart';
@@ -27,9 +34,6 @@ import 'package:learning_app/editor/models/editor_data_classes/link_tile_dc.dart
 import 'package:learning_app/editor/models/editor_data_classes/list_editor_tile_dc.dart';
 import 'package:learning_app/editor/models/editor_data_classes/quote_tile_dc.dart';
 import 'package:learning_app/editor/models/editor_data_classes/text_tile_dc.dart';
-import 'package:learning_app/ui_components/backend/hive_ui_api.dart';
-import 'package:learning_app/ui_components/backend/ui_repository.dart';
-import 'package:path_provider/path_provider.dart' as path_provider;
 
 Future<void> main() async {
   /// Init hive
@@ -51,24 +55,32 @@ Future<void> main() async {
     ..registerAdapter(LatexTileDCAdapter())
     ..registerAdapter(ListEditorTileDCAdapter())
     ..registerAdapter(QuoteTileDCAdapter())
+    ..registerAdapter(TextTileDCAdapter())
     ..registerAdapter(LinkTileDCAdapter())
-    ..registerAdapter(TextTileDCAdapter());
-  final cardsApi = HiveCardsApi(
-    await Hive.openBox<Subject>('subjects'),
-    await Hive.openBox<Folder>('folders'),
-    await Hive.openBox<Card>('cards'),
-    await Hive.openBox<List<String>>('relations'),
-    await Hive.openBox<List<EditorTileDC>>('card_content'),
-  );
+    ..registerAdapter(FrontBackSeparatorTileDCAdapter())
+    ..registerAdapter(CalendarDayAdapter());
 
+  final cardsApi = HiveCardsApi(
+      await Hive.openBox<Subject>('subjects'),
+      await Hive.openBox<Folder>('folders'),
+      await Hive.openBox<Card>('cards'),
+      await Hive.openBox<List<String>>('relations'),
+      await Hive.openBox<List<dynamic>>('card_content'),
+      await Hive.openBox<ClassTest>('class_tests'),
+      await Hive.openBox<List<String>>('subject_to_class_test'),
+      await Hive.openBox<List<String>>('dateToClassTest'));
   final cardsRepository = CardsRepository(cardsApi: cardsApi);
 
-  final uiApi = HiveUIApi(await Hive.openBox('hive_ui'));
-  final uiRepository = UIRepository(uiApi: uiApi);
+  final calendarApi = HiveCalendarApi(
+    await Hive.openBox<CalendarDay>('calendar_day'),
+    await Hive.openBox('calendar'),
+  );
+  final calendarRepository = CalendarRepository(calendarApi: calendarApi);
+
   await bootstrap(
     () => App(
       cardsRepository: cardsRepository,
-      uiRepository: uiRepository,
+      calendarRepository: calendarRepository,
     ),
   );
 }
